d0bcebb5ca6fa4429d9c6a71f8dd50f59fb6deb2
AndreaNova1 <69744573+AndreaNova1@users.noreply.github.com>
2022-06-11 13:59:23 -0600

    creacion del proyecto y creacion del login

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class SessionsController extends Controller
+{
+    public function create(){
+        return view('auth.login');
+    }
+    public function store(){
+        if(auth()->attempt(request(['email', 'password'])) == false){
+            return back()->withErrors([
+                'message' => 'El correo y clave son incorrectos, por favor verificar datos'
+            ]);
+        }
+        return redirect()->to('/menu');
+    }
+    public function destroy(){
+        auth()->logout();
+        return redirect()->to('/');
+    }
+    public function login(){
+        return view('auth.login');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionsController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [SessionsController::class,'login']);
+
+Route::get('/register', [RegisterController::class, 'create'])
+    ->middleware('guest')
+    ->name('register.index');
+
+Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+
+Route::get('/login', [SessionsController::class, 'create'])
+    ->middleware('guest')
+    ->name('login.index');
+
+Route::post('/', [SessionsController::class, 'store'])->name('login.store');
+
+Route::get('/logout', [SessionsController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('login.destroy');
